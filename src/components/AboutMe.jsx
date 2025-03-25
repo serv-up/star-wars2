04b0f52f55e2719df5b10dc68e2f7b3083a3d831
NexEnd <104933746+serv-up@users.noreply.github.com>
2025-03-25 21:25:06 +0100

    HM fertig

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {base_url} from "../utils/constants.js";
+import main from "../assets/main.jpg";
 
 const AboutMe = () => {
+    const [hero, setHero] = useState('Loading...');
+    useEffect(() => {
+        fetch(`${base_url}/v1/peoples/1`)
+            .then(res => res.json())
+            .then(data => {
+                setHero({
+                    name: data.name,
+                    gender: data.gender,
+                    height: data.height,
+                    eye_color: data.eye_color,
+                    mass: data.mass,
+                    birth_year: data.birth_year,
+                });
+            })
+            .catch(() => setHero('Error'))
+    })
     return (
-        <div>
-            About Me
+        <div className={'farGalaxy'}>
+            <img className="float-start w-25 me-3" src={main} alt="Hero"/>
+            <p>Name: {hero.name}</p>
+            <p>Gender: {hero.gender}</p>
+            <p>Height: {hero.height}</p>
+            <p>Eye color: {hero.eye_color}</p>
+            <p>Mass: {hero.mass}</p>
+            <p>Birth year: {hero.birth_year}</p>
         </div>
     );
 };
